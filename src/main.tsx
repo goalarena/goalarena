d10fd8acffd1cf2d6a6fc981d1f5491589d663ef
goalarena <mazlumtuncel446@gmail.com>
2026-03-27 17:58:59 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>GoalArena ⚽</h1>;
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>⚽ GoalArena</h1>
+      <p>Live football scores coming soon...</p>
+    </div>
+  );
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(

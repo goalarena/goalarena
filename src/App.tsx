e91d8f5f86de4b4387074b358f4b3662aa453c16
goalarena <mazlumtuncel446@gmail.com>
2026-03-27 18:36:24 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>⚽ GoalArena</h1>
+      <p>Live football scores coming soon...</p>
+    </div>
+  );
+}
+
+export default App;
